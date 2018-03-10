cf7dd9c4d1ed7ef791c027bf06997ad06a93228a
L. Garcia <lgarcia2013@fau.edu>
2018-03-09 20:27:24 -0500

    added comments
    Signed-off-by: FR3ED0M <lgarcia2013@fau.edu>

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ class openImage1:
 
 
 # choose watermark
+# images are resized down to 160 x 120
 class openImage2:
     def openPlains(self):
         wm = Image.open("images/plains.jpg")
@@ -116,8 +117,7 @@ class openImage2:
         return
 
 
-# creates a popup for the user to select
-# pictures from the those that come with the program
+# creates a popup for the user to select original image
 def do_popup(event):
     try:
         popup.tk_popup(event.x_root, event.y_root, 0)
@@ -125,6 +125,7 @@ def do_popup(event):
         popup.grab_release()
 
 
+# creates popup for user to choose the watermark image
 def do_popup2(event):
     try:
         popup2.tk_popup(event.x_root, event.y_root, 0)
@@ -133,59 +134,59 @@ def do_popup2(event):
 
 
 # embed a watermark into the original selected image
-def embedImage(z=5):
-    img = Image.open("images/new_image.png")
-    img = img.resize((250, 250), Image.ANTIALIAS)
-    wMark = Image.open("images/watermark.png").resize(img.size)
-    img.getpixel((0, 0))
-    wMark.getpixel((0, 0))
+def embedImage(z=5):                                                # value where images does become too saturated
+    img = Image.open("images/new_image.png")                        # open the original chosen image
+    img = img.resize((250, 250), Image.ANTIALIAS)                   # resize the image to be viewed in GUI
+    wMark = Image.open("images/watermark.png").resize(img.size)     # open watermark and resize the data to original
+    img.getpixel((0, 0))                                            # get pixel data for original image
+    wMark.getpixel((0, 0))                                          # get pixel data for watermark
 
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            a = img.getpixel((i, j))
-            b = wMark.getpixel((i, j))
-            red = (int(a[0])) - (int(a[0] % z)) + (int(z * b[0] / 255))
-            green = (int(a[1])) - (int(a[1] % z)) + (int(z * b[1] / 255))
-            blue = (int(a[2])) - (int(a[2] % z)) + (int(z * b[2] / 255))
-            img.putpixel((i, j), (red, green, blue))
+    for i in range(img.size[0]):                                    # loop inside the X range of original image
+        for j in range(img.size[1]):                                # loop inside the Y range of original image
+            a = img.getpixel((i, j))                                # a = (x,y) of original pixel data
+            b = wMark.getpixel((i, j))                              # b = (x,y) of watermark pixel data
+            red = (int(a[0])) - (int(a[0] % z)) + (int(z * b[0] / 255))     # algorithm for red RGB pixel data
+            green = (int(a[1])) - (int(a[1] % z)) + (int(z * b[1] / 255))   # algorithm for green RGB pixel data
+            blue = (int(a[2])) - (int(a[2] % z)) + (int(z * b[2] / 255))    # algorithm for blue RGB pixel data
+            img.putpixel((i, j), (red, green, blue))                # put watermark RGB values into last bits of (x,y)
 
-    ph2 = ImageTk.PhotoImage(img)
-    label3 = Label(image=ph2)
+    ph2 = ImageTk.PhotoImage(img)                                   # get embedded image to show in window
+    label3 = Label(image=ph2)                                       # put image into a label format from tkinter
     label3.image = ph2
-    label3.place(x=600, y=75)
-    img.save("images/inside.png")
+    label3.place(x=600, y=75)                                       # place image onto GUI window
+    img.save("images/inside.png")                                   # save the image to file
     return
 
 
 # extract the watermark from the original selected image
-# ERROR: ONLY SMALL SECTION OF WATERMARK IS GETTING EXTRACTED
+# image is extracted saturated more than its original state
 def xtractImage(z=5):
-    hidden = Image.open("images/inside.png")
+    hidden = Image.open("images/inside.png")                        # open image that has been watermarked
 
-    for i in range(hidden.size[0]):
-        for j in range(hidden.size[1]):
-            a = hidden.getpixel((i, j))
-            red = (int(a[0] % z) * (int(255 / z)))
-            green = (int(a[1] % z) * (int(255 / z)))
-            blue = (int(a[2] % z) * (int(255 / z)))
-            hidden.putpixel((i, j), (red, green, blue))
+    for i in range(hidden.size[0]):                                 # loop inside the X range of watermarked image
+        for j in range(hidden.size[1]):                             # loop inside the Y range of watermarked image
+            a = hidden.getpixel((i, j))                             # a = (x,y) of watermarked pixel data
+            red = (int(a[0] % z) * (int(255 / z)))                  # algorithm for decrypt of red RGB pixel data
+            green = (int(a[1] % z) * (int(255 / z)))                # algorithm for decrypt of green RGB pixel data
+            blue = (int(a[2] % z) * (int(255 / z)))                 # algorithm for decrypt of blue RGB pixel data
+            hidden.putpixel((i, j), (red, green, blue))             # get watermark RGB values from last bits of (x,y)
 
-    hidden = hidden.resize((160, 120), Image.ANTIALIAS)
-    ph3 = ImageTk.PhotoImage(hidden)
-    label4 = Label(image=ph3)
+    hidden = hidden.resize((160, 120), Image.ANTIALIAS)             # resize the watermark from 250x250 to 160x120
+    ph3 = ImageTk.PhotoImage(hidden)                                # get extracted watermark to show in window
+    label4 = Label(image=ph3)                                       # put watermark into a label format from tkinter
     label4.image = ph3
-    label4.place(x=890, y=75)
-    hidden.save("images/xtract.png")
+    label4.place(x=890, y=75)                                       # place image onto GUI window
+    hidden.save("images/xtract.png")                                # save the extracted watermark to file
     return
 
 
 # creates the popup menu to choose images from database
-x = openImage1()
-y = openImage2()
+x = openImage1()    # original images
+y = openImage2()    # watermark images
 
 # tear off the menu from the button
-popup = Menu(parent, tearoff=0)
-popup2 = Menu(parent, tearoff=0)
+popup = Menu(parent, tearoff=0)     # popup for selecting original images
+popup2 = Menu(parent, tearoff=0)    # popup for selecting watermark images
 
 # choice of 640 x 480 images
 popup.add_command(label="Boxy", command=x.openBoxy)
@@ -207,13 +208,13 @@ parent.title('Image Watermarking')
 # frame of the window
 frame = ttk.Frame(parent, borderwidth=5)
 
-# original and watermark selection buttons
+# original and watermark selection buttons with popup menus
 select = ttk.Button(parent, text="Select Image")
 select.bind("<Button-1>", do_popup)
 wmImage = ttk.Button(parent, text="Select Watermark")
 wmImage.bind("<Button-1>", do_popup2)
 
-# seperator
+# seperator between the original and watermark images & embedded and extraction images
 sep = ttk.Separator(parent, orient=VERTICAL)
 sep.pack(side=RIGHT, fill=Y, expand=True)
 
